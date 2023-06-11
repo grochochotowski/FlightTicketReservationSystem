ecf852a85839eec80e318c29b51a75e07e4f71ad
Dvrecky <hubert@DESKTOP-28MDB1H>
2023-06-11 17:00:43 +0200

    Make airport class

--- FlightTicketReservationSystem/Airport.cs
+++ FlightTicketReservationSystem/Airport.cs
@@ -5,6 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightTicketReservationSystem {
-    internal class Airport {
+
+    class Cordinates {
+        int x;
+        int y;
+
+        public Cordinates(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+     abstract class Airport {
+
+        protected string code;
+        protected Cordinates cordinates;
+        protected int NumberOfGates;
+        protected List<Runway> runways;
+
+        public void addRunway(Runway runway) {
+            runways.Add(runway);
+        }
+
+        public void removeRunway(Runway runway) {
+            runways.Remove(runway);
+        }
+
+        public List<Runway> GetRunways() {
+            return runways;
+        }
+
+        public Airport(string code, Cordinates cordinates, int NumberOfGates, List<Runway> runways) {
+            this.code = code;
+            this.cordinates = cordinates;
+            this.NumberOfGates = NumberOfGates;
+            this.runways = runways;
+        }
+    }
+
+    class Local : Airport {
+        public Local(string code, Cordinates cordinates, int NumberOfGates, List<Runway> runways) : base(code, cordinates, NumberOfGates, runways) { }
+    }
+
+    class International : Airport {
+        public International(string code, Cordinates cordinates, int NumberOfGates, List<Runway> runways) : base(code, cordinates, NumberOfGates, runways) { }
     }
 }
