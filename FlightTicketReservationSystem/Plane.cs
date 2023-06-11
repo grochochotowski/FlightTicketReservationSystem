953024214e7b96c3ce8599d31942d15035816faf
grochochotowski <gmich2002@wp.pl>
2023-06-11 16:40:01 +0200

    Make plane class

--- FlightTicketReservationSystem/Plane.cs
+++ FlightTicketReservationSystem/Plane.cs
@@ -5,11 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightTicketReservationSystem {
-    class Plane {
-        int x, y;
-        public Plane(int x, int y) {
-            this.x = x;
-            this.y = y;
+    abstract class Plane {
+        protected string number;
+        protected double width, length, maxDistance, speed;
+        protected int numberOfSeats;
+        public Plane(string number, double width, double length, double maxDistance, double speed, int numberOfSeats) {
+            this.number = number;
+            this.width = width;
+            this.length = length;
+            this.maxDistance = maxDistance;
+            this.speed = speed;
+            this.numberOfSeats = numberOfSeats;
         }
+    }
+    class LightJet: Plane {
+        public LightJet(string number, double width, double length, double maxDistance, double speed, int numberOfSeats): base(number, width, length, maxDistance, speed, numberOfSeats) { }
+    }
+    class MidSizeJet: Plane {
+        public MidSizeJet(string number, double width, double length, double maxDistance, double speed, int numberOfSeats): base(number, width, length, maxDistance, speed, numberOfSeats) { }
+    }
+    class JumboJet: Plane {
+        public JumboJet(string number, double width, double length, double maxDistance, double speed, int numberOfSeats): base(number, width, length, maxDistance, speed, numberOfSeats) { }
+    }
+    class BussinessJet: Plane {
+        public BussinessJet(string number, double width, double length, double maxDistance, double speed, int numberOfSeats): base(number, width, length, maxDistance, speed, numberOfSeats) { }
     }
 }
