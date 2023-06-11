0c583d5974e91cb8c66949c19d039eeca1cc01aa
Jakub Gerwel <kuba.gerwel@gmail.com>
2023-06-11 16:46:47 +0200

    Make clinet class

--- FlightTicketReservationSystem/Client.cs
+++ FlightTicketReservationSystem/Client.cs
@@ -6,5 +6,53 @@ using System.Threading.Tasks;
 
 namespace FlightTicketReservationSystem {
     internal class Client {
+        protected string phoneNumber;
+        protected string email;
+        protected List<Ticket> tickets = new List<Ticket>();
+
+            public Client(string phoneNumber, string email)
+        {
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public void addTicket(Ticket ticket)
+        {
+            tickets.Add(ticket);
+
+        }
+        public void removeTicket(Ticket ticket)
+        {
+            tickets.Remove(ticket);
+        }
+        public List<Ticket> getTickets()
+        {
+            return tickets;
+        }
+    }
+
+    class Person : Client
+    {
+        private string firstName;
+        private string SecondName;
+        private string lastNmae;
+        private int age;
+        public Person(string firstName, string SecondName, string lastName,  string phoneNumber, string email) : base(phoneNumber, email)   
+        { 
+            this.firstName = firstName;
+            this.SecondName = SecondName;
+            this.lastNmae = lastName;
+        }
+    }
+
+    class Company : Client
+    {
+        private string name;
+        private string krs;
+        public Company(string name, string krs, string phoneNumber, string email) : base(phoneNumber, email) 
+        {
+            this.name = name;
+            this.krs = krs;
+        }
     }
 }
