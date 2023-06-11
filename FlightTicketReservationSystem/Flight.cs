fa3bf7052f7a996a57141e1a068132b4190d663b
grochochotowski <gmich2002@wp.pl>
2023-06-11 17:22:29 +0200

    Make flight class

--- FlightTicketReservationSystem/Flight.cs
+++ FlightTicketReservationSystem/Flight.cs
@@ -6,5 +6,26 @@ using System.Threading.Tasks;
 
 namespace FlightTicketReservationSystem {
     internal class Flight {
+        private Route route;
+        private Plane plane;
+        private string departureDate, arrivalDate, gateNumebr; // date format: DD/MM/YYY - Hr.Min.Sec
+        private double duration;
+        public Flight(Route route, Plane plane, string departureDate, string arrivalDate, string gateNumber, double douration) {
+            this.route = route;
+            this.plane = plane;
+            this.departureDate = departureDate;
+            this.arrivalDate = arrivalDate;
+            this.gateNumebr = gateNumber;
+            this.duration = calculateDuration(departureDate, arrivalDate);
+        }
+        public double calculateDuration(string departureDateString, string arrivalDateString) {
+            DateTime arrivalDate = DateTime.ParseExact(arrivalDateString, "dd/MM/yyyy - H.mm.ss", null);
+            DateTime departureDate = DateTime.ParseExact(departureDateString, "dd/MM/yyyy - H.mm.ss", null);
+
+            TimeSpan duration = arrivalDate - departureDate;
+
+            double totalMinutes = duration.TotalMinutes;
+            return totalMinutes;
+        }
     }
 }
