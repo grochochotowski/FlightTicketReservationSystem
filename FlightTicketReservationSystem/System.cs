90f771bb1e654e04a5a99428261815dd0c235b6a
grochochotowski <gmich2002@wp.pl>
2023-06-11 18:06:25 +0200

    Fix system class

--- FlightTicketReservationSystem/System.cs
+++ FlightTicketReservationSystem/System.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace FlightTicketReservationSystem {
     internal class System {
-        private List<Client> clients = new List<Client>();   
+        private List<Client> clients = new List<Client>();
         private List<Route> routes = new List<Route>();
         private List<Airport> airports = new List<Airport>();
         private List<Plane> planes = new List<Plane>();
 
         public void addClient(Client client) { clients.Add(client); }
-      
+
         public void removeClient(Client client) { clients.Remove(client); }
-        
+
         public List<Client> getClient() { return clients; }
 
 
-        
+
         public void addRoute(Route route) { routes.Add(route); }
 
         public void removeRoute(Route route) { routes.Remove(route); }
@@ -27,19 +27,19 @@ namespace FlightTicketReservationSystem {
 
 
 
-        public void addAirport(Airport airport) { airports.Add(airport);}
-            
-        public void removeAirport(Airport airport) { airports.Remove(airport);}
+        public void addAirport(Airport airport) { airports.Add(airport); }
+
+        public void removeAirport(Airport airport) { airports.Remove(airport); }
 
         public List<Airport> getAirports() { return airports; }
 
 
 
-        public void addPlane(Plane plane) {  planes.Add(plane);}
+        public void addPlane(Plane plane) { planes.Add(plane); }
 
-        public void removePlane(Plane plane) {  planes.Remove(plane);}
+        public void removePlane(Plane plane) { planes.Remove(plane); }
 
-        public List<Plane> getPlanes() { return planes;}
+        public List<Plane> getPlanes() { return planes; }
 
-
+    }
 }
