60c243f9b68f453570f1bb54df221f684c390c49
Jakub Gerwel <kuba.gerwel@gmail.com>
2023-06-15 07:41:53 +0200

    All constructors

--- FlightTicketReservationSystem/Client.cs
+++ FlightTicketReservationSystem/Client.cs
@@ -9,9 +9,18 @@ namespace FlightTicketReservationSystem {
         protected string phoneNumber;
         protected string email;
         protected List<Ticket> tickets = new List<Ticket>();
+        protected Person person;
+        protected Company company;
 
-            public Client(string phoneNumber, string email)
+        public Client(Person person,  string phoneNumber, string email)
+        {   
+            this.person = person;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        public Client(Company comapny, string phoneNumber, string email)
         {
+            this.company = company;
             this.phoneNumber = phoneNumber;
             this.email = email;
         }
@@ -37,6 +46,14 @@ namespace FlightTicketReservationSystem {
         private string SecondName;
         private string lastNmae;
         private int age;
+        private string surname;
+
+        public Person(string phoneNumber, string email, string surname, int age) : base(phoneNumber, email)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
         public Person(string firstName, string SecondName, string lastName,  string phoneNumber, string email) : base(phoneNumber, email)   
         { 
             this.firstName = firstName;
@@ -49,6 +66,11 @@ namespace FlightTicketReservationSystem {
     {
         private string name;
         private string krs;
+
+        public Company(string phoneNumber, string email) : base(phoneNumber, email)
+        {
+        }
+
         public Company(string name, string krs, string phoneNumber, string email) : base(phoneNumber, email) 
         {
             this.name = name;
